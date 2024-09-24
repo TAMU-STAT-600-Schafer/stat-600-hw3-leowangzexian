4153de950e49e223c13e9627deb78f9bdeadb02b
Leo Zexian Wang <leowang@tamu.edu>
2024-09-24 02:25:50 -0500

    performed more tests on the function LRMultiClass in Tests.R

--- Tests.R
+++ Tests.R
@@ -3,9 +3,9 @@ source("FunctionsLR.R")
 
 # first test case
 Y = c(0, 1, 2, 3, 4, 3, 2, 1, 0, 2, 3, 4, 1, 2, 0, 4)
-X = matrix(rnorm(15*20), 15)
+X = matrix(rnorm(16*19), 15)
 Yt = c(1, 0, 3, 2)
-Xt = matrix(rnorm(3*20), 3)
+Xt = matrix(rnorm(4*19), 4)
 X = cbind(1, X)
 Xt = cbind(1, Xt)
 out = LRMultiClass(X, Y, Xt, Yt, numIter = 50, eta = 0.1, lambda = 1)
@@ -34,11 +34,11 @@ plot(out$error_train, type = 'o', xlab = "number of iterations", ylab = "trainin
 plot(out$error_test, type = 'o')
 
 # compatibility checks to see if error messages will be returned appropriately
-out = LRMultiClass(matrix(rnorm(16*20), 16), Y, Xt, Yt, numIter = 50, eta = 0.1, lambda = 1)
-out = LRMultiClass(X, Y, matrix(rnorm(4*20), 4), Yt, numIter = 50, eta = 0.1, lambda = 1)
+out = LRMultiClass(matrix(rnorm(17*20), 17), Y, Xt, Yt, numIter = 50, eta = 0.1, lambda = 1)
+out = LRMultiClass(X, Y, matrix(rnorm(5*20), 5), Yt, numIter = 50, eta = 0.1, lambda = 1)
 out = LRMultiClass(X, c(0, 1, 2, 3), Xt, Yt, numIter = 50, eta = 0.1, lambda = 1)
 out = LRMultiClass(X, Y, Xt, c(1, 2), numIter = 50, eta = 0.1, lambda = 1)
-out = LRMultiClass(X, Y, matrix(rnorm(4*10), 4), Yt, numIter = 50, eta = 0.1, lambda = 1)
+out = LRMultiClass(X, Y, matrix(rnorm(4*11), 4), Yt, numIter = 50, eta = 0.1, lambda = 1)
 out = LRMultiClass(X, Y, Xt, Yt, numIter = 50, eta = -0.1, lambda = 1)
 out = LRMultiClass(X, Y, Xt, Yt, numIter = 50, eta = 0.1, lambda = -1)
 out = LRMultiClass(X, Y, Xt, Yt, numIter = 50, eta = 0.1, lambda = 1, beta_init = matrix(0, 20, 2))
@@ -46,9 +46,9 @@ out = LRMultiClass(X, Y, Xt, Yt, numIter = 50, eta = 0.1, lambda = 1, beta_init 
 
 # 2nd test case
 Y = rbinom(1000, size = 10, prob = 0.5) 
-X = matrix(rnorm(999*500), 999)
+X = matrix(rnorm(1000*499), 1000)
 Yt = rbinom(200, size = 10, prob = 0.5) 
-Xt = matrix(rnorm(199*500), 199)
+Xt = matrix(rnorm(200*499), 200)
 X = cbind(1, X)
 Xt = cbind(1, Xt)
 out = LRMultiClass(X, Y, Xt, Yt, numIter = 50, eta = 0.1, lambda = 1)
